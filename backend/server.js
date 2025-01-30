b894b4824e08a4edfca78ad0b4bd9e1d2db82346
tanishqv07 <tanishqvyas036@gmail.com>
2025-01-30 23:35:03 +0530

    removed stripe integration

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ const paymentRoutes = require("./routes/paymentRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
-app.use("/api/payment", paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
