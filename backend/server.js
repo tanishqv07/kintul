95def12b1224f9bef7cea9b7c95c6da62c933748
tanishqv07 <tanishqvyas036@gmail.com>
2025-01-31 11:25:48 +0530

    testing routes

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,8 @@ const bookingRoutes = require("./routes/bookingRoutes");
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
 
+app.get("/", (req, res) => {
+    res.send("Backend is working!");
+  });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
