9d1a3096bc07d2cf628783089fed431738ff20ee
tanishqv07 <tanishqvyas036@gmail.com>
2025-01-31 20:12:05 +0530

    multipart

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { login, register } = require("../controllers/authController");
-
+const upload = require("../middleware/uploadMiddleware");
 const router = express.Router();
 
 // User Registration
