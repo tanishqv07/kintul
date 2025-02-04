5d451b55990ee0a3d9e2a2ca23810c0bd1c189bb
tanishqv07 <tanishqvyas036@gmail.com>
2025-02-04 16:22:52 +0530

    minor bug fix

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors()); //cross orienting requests
 //routes
 const authRoutes = require("./routes/authRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
-
+const serviceRoutes = require("./routes/serviceRoutes");
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/services",serviceRoutes);
