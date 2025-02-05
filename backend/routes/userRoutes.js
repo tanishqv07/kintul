572c39cf2b023d95fedee2da1646ea8913761ecc
tanishqv07 <tanishqvyas036@gmail.com>
2025-02-05 15:27:15 +0530

    fixed userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const { getProfile, updateProfile } = require("../controllers/userController");
-const authMiddleware = require("../middleware/authMiddleware"); // Ensure user is authenticated
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// Get user profile (requires authentication)
+//routes
 router.get("/profile", authMiddleware, getProfile);
-
-// Update user address
 router.put("/profile", authMiddleware, updateProfile);
 
 module.exports = router;
