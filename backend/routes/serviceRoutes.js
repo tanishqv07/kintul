3553190351b6b7886880a0318e190fefdfc8b277
tanishqv07 <tanishqvyas036@gmail.com>
2025-02-06 16:49:43 +0530

    minor bug

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const { getServices, addService, deleteService } = require("../controllers/serviceController");
-const {authMiddleware, roleMiddleware} = require("../middleware/authMiddleware")
 const upload = require("../middleware/uploadMiddleware");
 const router = express.Router();
 
 //Get all services
-router.get("/", authMiddleware,roleMiddleware("customer"), getServices);
+router.get("/", getServices);
 
 //Add new service
 router.post("/",upload.single('image'), addService);
